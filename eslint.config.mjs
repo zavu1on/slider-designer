886e6093bf308f7065154717cc50f13540f80c7b
Mikhail <joinmma@gmail.com>
2025-02-21 23:23:28 +0300

    created base configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
+
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
@@ -10,7 +11,47 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    'next/core-web-vitals',
+
+    'next/typescript',
+
+    'plugin:tailwindcss/recommended',
+
+    'plugin:prettier/recommended',
+
+    'plugin:import/typescript',
+
+    'plugin:@typescript-eslint/recommended',
+
+    'prettier'
+  ),
+
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+
+    rules: {
+      'tailwindcss/no-custom-classname': 'off',
+
+      'prettier/prettier': [
+        'error',
+
+        {
+          endOfLine: 'auto',
+        },
+      ],
+
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+
+        {
+          prefer: 'type-imports',
+
+          fixStyle: 'inline-type-imports',
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
